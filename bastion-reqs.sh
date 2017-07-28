4d14510e9352606e0ef80ebef3bb1d3e8b837e14
Francisco Moctezuma <francisco.moctezuma@rackspace.com>
2017-07-28 14:45:26 -0500

    Create bastion-reqs.sh

---
+++ bastion-reqs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+sleep 2m
+
+BLACKLIST="/dev/vda|/dev/sda"
+DEV=($(sudo ls -1 /dev/vd*|egrep -v "${BLACKLIST}"|egrep -v "[0-9]$"))
+sudo mkdir /managed-k8s
+echo "n
+p
+1
+
+
+w
+"| sudo fdisk "${DEV}" > /dev/null 2>&1
+
+MOUNTPOINT=/managed-k8s
+PARTITION=$(sudo fdisk -l ${DEV}|grep -A 1 Device|tail -n 1|awk '{print $1}')
+        echo "Creating filesystem on ${PARTITION}."
+        sudo mkfs -F -j -t ext4 ${PARTITION}
+        sudo mount ${PARTITION} ${MOUNTPOIN}
+
+    read UUID FS_TYPE < <(sudo blkid -u filesystem ${PARTITION}|awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+
+add_to_fstab() {
+    UUID=${1}
+    MOUNTPOINT=${2}
+    grep "${UUID}" /etc/fstab >/dev/null 2>&1
+    if [ ${?} -eq 0 ];
+    then
+        echo "${UUID} already exists"
+    else
+        LINE="UUID=\"${UUID}\"\t${MOUNTPOINT}\text4\tnoatime,nodiratime,nodev,noexec,nosuid\t1 2"
+        sudo echo -e "${LINE}" >> /etc/fstab
+    fi
+}
+
+    add_to_fstab "${UUID}" "${MOUNTPOINT}"
+    echo "Mounting disk ${PARTITION} on ${MOUNTPOINT}"
+    sudo mount "${MOUNTPOINT}"
+
+# Installing pre-reqs
+
+DOCKER_VERSION=17.03.1
+
+# Docker
+curl -Lo /tmp/docker.tgz \
+    https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}-ce.tgz
+sudo tar --extract --file /tmp/docker.tgz --strip-components 1 --directory /usr/local/bin/ >/dev/null 2>&1
