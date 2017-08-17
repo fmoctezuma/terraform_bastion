c736623d29988ac3f0ffd7ef8a943b86dcfef25f
Francisco Moctezuma <francisco.moctezuma@rackspace.com>
2017-08-17 14:34:35 -0500

    Adding some resources
    • adding null resource on terraform to execute the script once volume was mapped
    • adding security_group resource
    • adding bastion_port resource
    • removing docker client and installing dockerio from ubuntu repo
    • adding user ubuntu on docker group

--- deploy.tf
+++ deploy.tf
@@ -1,19 +1,32 @@
-variable "count" {
-  default = 1
-}
-
-variable "diskn" {
-  default = 1
-}
-
-variable "disk_size" {
-  default = 10
-}
-
 resource "openstack_blockstorage_volume_v2" "bastion_volume" {
   count = "${var.diskn}"
   name  = "${format("bastion_disk%02d", count.index)}"
   size  = "${var.disk_size}"
+}
+
+resource "openstack_networking_port_v2" "bastion_port" {
+  name           = "bastion_port"
+  admin_state_up = "true"
+  network_id = "${var.network_id}"
+}
+
+resource "openstack_compute_secgroup_v2" "bastion_secgroup" {
+  name        = "bastion_secgroup"
+  description = "Bastion Security Group"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
 }
 
 resource "openstack_compute_instance_v2" "bastion_server" {
@@ -22,9 +35,10 @@ resource "openstack_compute_instance_v2" "bastion_server" {
   image_name = "${var.image_name}"
   flavor_id = "${var.flavor_id}"
   key_pair = "${var.openstack_keypair}"
-  security_groups = ["default"]
+  security_groups = ["${openstack_compute_secgroup_v2.bastion_secgroup.name}"]
   network {
     name = "${var.tenant_network}"
+    port = "${openstack_networking_port_v2.bastion_port.id}"
     }
 
   provisioner "file" {
@@ -33,26 +47,30 @@ resource "openstack_compute_instance_v2" "bastion_server" {
 
   connection {
       type     = "ssh"
-      user     = "your user"
-      private_key = "${var.private_key}"
-      }
-  }
-
-  provisioner "remote-exec" {
-     inline = [
-     "sudo bash /tmp/bastion-reqs.sh >/dev/null 2>&1 &",
-     ]
-
-  connection {
-      type     = "ssh"
       user     = "ubuntu"
       private_key = "${var.private_key}"
-             }
       }
-
-}
+   }
+  }
 
 resource "openstack_compute_volume_attach_v2" "volume_attach" {
   instance_id = "${openstack_compute_instance_v2.bastion_server.*.id[count.index]}"
   volume_id   = "${openstack_blockstorage_volume_v2.bastion_volume.*.id[count.index]}"
+
+}
+
+resource "null_resource" "bastion_reqs" {
+  depends_on = ["openstack_compute_volume_attach_v2.volume_attach"]
+  provisioner "remote-exec" {
+     inline = [
+     "sudo bash /tmp/bastion-reqs.sh",
+     ]
+
+  connection {
+    host = "${openstack_compute_instance_v2.bastion_server.network.0.fixed_ip_v4}"
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${var.private_key}"
+      }
+   }
 }
