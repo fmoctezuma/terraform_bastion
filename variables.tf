3612a771838b792309a1938a0e2aa28a8ca9a87e
Francisco Moctezuma <francisco.moctezuma@rackspace.com>
2017-08-18 09:45:53 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,58 +10,17 @@ variable "disk_size" {
   default = 50
 }
 
-variable "openstack_user_name" {
-    description = "The username for the Tenant."
-    default  = "your user"
-}
-
-variable "openstack_tenant_name" {
-    description = "The name of the Tenant."
-    default  = "your tenant"
-}
-
-variable "openstack_password" {
-    description = "The password for the Tenant."
-    default  = "your pass"
-}
-
-variable "openstack_auth_url" {
-    description = "The endpoint url to connect to OpenStack."
-    default  = "${OS_PUB_ENDPOINT}"
-}
-
-variable "openstack_keypair" {
-    description = "The keypair to be used."
-    default  = "yourkey"
-}
-
 variable "tenant_network" {
-    description = "The network to be used."
-    default  = "public"
+  description = "The network to be used."
+  default     = "public"
 }
-
-variable "network_id" {
-    description = "network id"
-    default = "baa13aba-35da-42e8-99ff-7aa9d48586e6"
-}
-
 
 variable "image_name" {
-    description = "Glance image name."
-    default = "Ubuntu Server 16.04 LTS Xenial Xerus (cloudimg)"
+  description = "Glance image name."
+  default     = "Ubuntu Server 16.04 LTS Xenial Xerus (cloudimg)"
 }
 
-variable "flavor_id" {
-    description = "Openstack flavor id."
-    default = "e46a21da-3487-4fac-8e16-d1239f41f58b"
-}
-
-variable "private_key" {
-    type = "string"
-    description = "private_key"
-    default = <<EOF
------BEGIN RSA PRIVATE KEY-----
-your key
------END RSA PRIVATE KEY-----
-EOF
+variable "flavor_name" {
+  description = "Openstack flavor name."
+  default     = "m1.medium"
 }
